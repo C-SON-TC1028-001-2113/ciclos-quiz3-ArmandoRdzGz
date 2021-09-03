854d3cca44a386f64430cc73e04d7dd98e7297ca
ArmandoRdzGz <A01254249@itesm.mx>
2021-09-03 16:39:27 +0000

    Repeticiones_while

--- assignments/05PromedioSencillo/src/exercise.py
+++ assignments/05PromedioSencillo/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    n = int(input(''))
+    c = 0
+    prom = 0
+    valores = 0
+    while n > c:
+        valores = valores + float(input(''))
+        c = c+1
+    prom = valores/n
+    
+    print (prom)  
+        
 
 if __name__=='__main__':
     main()
